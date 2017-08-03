0e9cad0f9c79ff1bd567f48264a4670016790418
Yusuf Ebrahim <yusufmte@gmail.com>
2017-08-03 16:53:44 -0400

    added matches

--- peung.py
+++ peung.py
@@ -10,26 +10,36 @@ MAX_SOUND_FILE = 30 # maximum score for which a recording exists
 QLUMB_MODE = False # toggle qlumbers
 SPECIAL_PLAYERS = ["heddood", "yusuf", "yussra", "mama", "baba"]
 OUTCOME_MAPPING = {Button.left:0, Button.right:1, Button.middle:-1}
+GAME_LENGTH = 11 # number of rallies in a game
+
+
+def say_num(num):
+  num_prefix = "num" if not QLUMB_MODE else "qlum"
+  playsound(num_prefix+"_"+str(num)+".mp3")
+
 
 class Game:
 
-  def __init__(self,player0,player1):
+  def __init__(self,player):
     self.score=[0,0]
     self.server = random.choice([0,1])
     self.serve_counter=0
     self.deuce = False
-    self.player = [player0,player1]
+    self.player = player
 
   def report(self):
     for i in range(2):
       print self.player[i]+": "+str(self.score[i])
     print self.player[self.server]+"'s serve."
 
+    if sum(self.score)==0:
+      playsound("game_score.mp3")
+
     if max(self.score) <= MAX_SOUND_FILE:
       num_prefix = "num" if not QLUMB_MODE else "qlum"
-      playsound(num_prefix+"_"+str(self.score[0])+".mp3")
-      sleep(0.35)
-      playsound(num_prefix+"_"+str(self.score[1])+".mp3")
+      say_num(self.score[0])
+      sleep(0.2)
+      say_num(self.score[1])
     sleep(0.65)
     if self.player[self.server] in SPECIAL_PLAYERS:
       playsound("serve_"+str(self.player[self.server])+".mp3")
@@ -43,7 +53,7 @@ class Game:
     else:
       self.previous = copy.deepcopy(self)
       self.score[outcome]+=1
-      if self.score[0]==10 and self.score[1]==10:
+      if self.score[0]==GAME_LENGTH-1 and self.score[1]==GAME_LENGTH-1:
         self.deuce = True
         playsound("deuce.mp3")
       if self.serve_counter == 1 or self.deuce:
@@ -59,6 +69,21 @@ class Game:
       playsound("you_peuped.mp3")
       playsound("peuped.mp3")
       self.__dict__ = self.previous.__dict__
+
+  def play(self):
+    while abs(self.score[0]-self.score[1])<2 or max([self.score[0],self.score[1]]) < GAME_LENGTH:
+      self.report()
+      print "Left click if "+self.player[0]+" scores and right click if "+self.player[1]+" scores. Middle click to undo."
+      print ""
+      self.next()
+    
+    winner = 0 if self.score[0] > self.score[1] else 1
+    if self.player[winner] in SPECIAL_PLAYERS:
+      playsound("win_"+str(player[winner])+".mp3")
+    else:
+      playsound("win_"+str(winner)+".mp3")
+    playsound("factorio.mp3")
+    return winner
 
     
 def on_click(x,y,button,pressed):
@@ -76,28 +101,31 @@ def set_outcome():
     sleep(0.1)
   listener.stop()
 
+print "How many games in this match?"
+num_games = int(raw_input())
+if num_games%2==0: num_games+=1
+player=[]
+print "Enter player 0 name."
+player.append(raw_input())
+print "Enter player 1 name."
+player.append(raw_input())
 
-print "Enter a player name."
-player0 = raw_input()
-print "Enter a player name."
-player1 = raw_input()
+match_score = [0,0]
 
-game = Game(player0,player1)
+while max(match_score) <= num_games/2:
+  game = Game(player)
+  playsound("match_score.mp3")
+  say_num(match_score[0])
+  sleep(0.2)
+  say_num(match_score[1])
+  sleep(0.65)
+  match_score[game.play()] += 1
 
-
-while abs(game.score[0]-game.score[1])<2 or max([game.score[0],game.score[1]]) < 11:
-  game.report()
-  print "Left click if "+game.player[0]+" scores and right click if "+game.player[1]+" scores. Middle click to undo."
-  print ""
-  game.next()
-
-
-game.report()
-
-
-winner = 0 if game.score[0] > game.score[1] else 1
-if game.player[winner] in SPECIAL_PLAYERS:
-  playsound("win_"+str(player[winner])+".mp3")
+match_winner = 0 if match_score[0] > match_score[1] else 1
+if player[match_winner] in SPECIAL_PLAYERS:
+  playsound("win_"+str(player[match_winner])+".mp3")
 else:
-  playsound("win_"+str(winner)+".mp3")
-playsound("factorio.mp3")
+  playsound("win_"+str(match_winner)+".mp3")
+sleep(0.5)
+playsound("the_match.mp3")
+playsound("match_factorio.mp3")
