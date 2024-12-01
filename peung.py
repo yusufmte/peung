160f178f0604bbf1fd5e2d5a3846931c4bf132b5
Yusuf Ebrahim <yusufmte@gmail.com>
2024-12-01 00:33:56 -0500

    switch from playsound to pygame.mixer #3

--- peung.py
+++ peung.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 
-import random, copy
+import random, copy, pygame
 from pynput import mouse
 from time import sleep
-from playsound import playsound
 
 
 MAX_SOUND_FILE = 30 # maximum score for which a recording exists
-QLUMB_MODE = False # toggle qlumbers
+QLUMB_MODE = True # toggle qlumbers
 SPECIAL_PLAYERS = ["heddood", "yusuf", "yussra", "mama", "baba"]
 GAME_LENGTH = 11 # number of rallies in a game
 
@@ -18,9 +17,15 @@ OUTCOME_MAPPING = {
 }
 
 
+def play_sound(file):
+  pygame.mixer.music.load(file)
+  pygame.mixer.music.play()
+  while pygame.mixer.music.get_busy():
+    pygame.time.Clock().tick(10)
+
 def say_num(num):
   num_prefix = "num" if not QLUMB_MODE else "qlum"
-  playsound(num_prefix+"_"+str(num)+".mp3")
+  play_sound(num_prefix+"_"+str(num)+".mp3")
 
 
 class Game:
@@ -38,7 +43,7 @@ class Game:
     print(self.player[self.server]+"'s serve.")
 
     if sum(self.score)==0:
-      playsound("game_score.mp3")
+      play_sound("game_score.mp3")
 
     if max(self.score) <= MAX_SOUND_FILE:
       say_num(self.score[0])
@@ -46,9 +51,9 @@ class Game:
       say_num(self.score[1])
     sleep(0.65)
     if self.player[self.server] in SPECIAL_PLAYERS:
-      playsound("serve_"+str(self.player[self.server])+".mp3")
+      play_sound("serve_"+str(self.player[self.server])+".mp3")
     else:
-      playsound("serve_"+str(self.server)+".mp3")
+      play_sound("serve_"+str(self.server)+".mp3")
 
   def next(self):
     set_outcome()
@@ -59,7 +64,7 @@ class Game:
       self.score[outcome]+=1
       if self.score[0]==GAME_LENGTH-1 and self.score[1]==GAME_LENGTH-1:
         self.deuce = True
-        playsound("deuce.mp3")
+        play_sound("deuce.mp3")
       if self.serve_counter == 1 or self.deuce:
         self.server = 1 if self.server == 0 else 0
         self.serve_counter = 0
@@ -68,10 +73,10 @@ class Game:
   
   def undo(self):
     if "previous" not in self.__dict__.keys():
-      playsound("you_peuped.mp3")
+      play_sound("you_peuped.mp3")
     else:
-      playsound("you_peuped.mp3")
-      playsound("peuped.mp3")
+      play_sound("you_peuped.mp3")
+      play_sound("peuped.mp3")
       self.__dict__ = self.previous.__dict__
 
   def play(self):
@@ -82,10 +87,10 @@ class Game:
     
     winner = 0 if self.score[0] > self.score[1] else 1
     if self.player[winner] in SPECIAL_PLAYERS:
-      playsound("win_"+str(player[winner])+".mp3")
+      play_sound("win_"+str(player[winner])+".mp3")
     else:
-      playsound("win_"+str(winner)+".mp3")
-    playsound("factorio.mp3")
+      play_sound("win_"+str(winner)+".mp3")
+    play_sound("factorio.mp3")
     return winner
 
     
@@ -101,6 +106,8 @@ def set_outcome():
   with mouse.Listener(on_click=on_click) as listener:
     listener.join()
 
+pygame.init()
+pygame.mixer.init()
 print("How many games in this match?")
 num_games = int(input())
 if num_games%2==0: num_games+=1
@@ -114,7 +121,7 @@ match_score = [0,0]
 
 while max(match_score) <= num_games/2:
   game = Game(player)
-  playsound("match_score.mp3")
+  play_sound("match_score.mp3")
   say_num(match_score[0])
   sleep(0.2)
   say_num(match_score[1])
@@ -123,9 +130,9 @@ while max(match_score) <= num_games/2:
 
 match_winner = 0 if match_score[0] > match_score[1] else 1
 if player[match_winner] in SPECIAL_PLAYERS:
-  playsound("win_"+str(player[match_winner])+".mp3")
+  play_sound("win_"+str(player[match_winner])+".mp3")
 else:
-  playsound("win_"+str(match_winner)+".mp3")
+  play_sound("win_"+str(match_winner)+".mp3")
 sleep(0.5)
-playsound("the_match.mp3")
-playsound("match_factorio.mp3")
+play_sound("the_match.mp3")
+play_sound("match_factorio.mp3")
